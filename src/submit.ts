2e8404374ca433273f8c65974930ef140b178d3b
Jase Thomson <jasethomson31@gmail.com>
2026-09-02 08:40:38 -0700

    update input for level and ans to match year/day structure and readme

--- src/submit.ts
+++ src/submit.ts
@@ -6,8 +6,9 @@ import { submitAnswer } from './utils';
 
 (async () => {
     const yearDayRegex = /year=(\d{4})day=(\d{1,2})/;
-    if (process.argv.length !== 5 || yearDayRegex.test(process.argv[3])) {
-        console.warn('Require year and day input in this format: year=xxxxday=xx, please retry.');
+    const ansRegex = /level=(\d)ans=(.+)/;
+    if (process.argv.length !== 4 || !yearDayRegex.test(process.argv[2]) || !ansRegex.test(process.argv[3])) {
+        console.warn('Require year and day input in this format: year=xxxxday=xx, level and answer input in this format: level=xans=xxx, please retry.');
         return;
     }
 
@@ -20,8 +21,15 @@ import { submitAnswer } from './utils';
     const year = parseInt(yearDayMatch[1]);
     const day = parseInt(yearDayMatch[2]);
 
-    const level = process.argv[3];
-    const answer = process.argv[4];
+    const ansMatch = process.argv[3].match(ansRegex);
+
+    if (!ansMatch?.[1] || !ansMatch[2]) {
+        console.warn('Invalid input found for answer input');
+        return;
+    }
+
+    const level = ansMatch[1];
+    const answer = ansMatch[2];
 
     const answerResHtml = await submitAnswer({ day, year, level, answer });
 
@@ -35,7 +43,7 @@ import { submitAnswer } from './utils';
         Submitted answer ${answer} for year ${year}, day ${day}, level ${level}.
         Answer Response:
         ____________________
-        
+
         ${$(article).text()}
         ____________________
     `)
